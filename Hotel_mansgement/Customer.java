5a8fac6d30d077e1ce3f9f018649deac495f0822
Yingluomm <153310424+Yingluomm@users.noreply.github.com>
2024-12-11 21:16:53 +0800

    Update Customer.java

--- Hotel_mansgement/Customer.java
+++ Hotel_mansgement/Customer.java
@@ -1,72 +1,72 @@
 package Hotel_mansgement;
 
-import java.util.Objects;
+import java.util.Scanner;
 
-public class Customer {
-    public static final int MAX=50;
-    public static Customer[] arrCustomer=new Customer[MAX];
-    public static int index=0;
-    private Integer roomId;
-    private String name;
-    private String gender;
-    private String idNumber;
-    public Customer()
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("顾客你好,本店所有房间都为空");
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("---------------------商务酒店管理系统---------------------------------");
+            System.out.println("1.添加顾客信息");
+            System.out.println("2.修改顾客信息");
+            System.out.println("3.删除顾客信息");
+            System.out.println("4.查看顾客信息");
+            System.out.println("5.退出");
+            System.out.println("请选择(1-5):");
+            int choice=sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    add();
+                    break;
+                case 2:
+                    modify();
+                    break;
+                case 3:
+                    delete();
+                    break;
+                case 4:
+                    view();
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("输入错误，请重新输入");
+            }
+        }
+    }
+    public static void add()
+    {
+        if(Customer.index>=Customer.arrCustomer.length)
+        {
+            System.out.println("无空房间");
+            return;
+        }
+        Scanner sc=new Scanner(System.in);
+        System.out.println("房间号为(1-50):");
+        int roomId=sc.nextInt();
+        System.out.println("请输入姓名:");
+        String name=sc.next();
+        System.out.println("请输入性别(男/女):");
+        String gender=sc.next();
+        System.out.println("请输入身份证号码:");
+        String idNumber=sc.next();
+        Customer.arrCustomer[Customer.index]=new Customer(roomId,name,gender,idNumber);
+        System.out.println(Customer.arrCustomer[Customer.index].toString());
+        Customer.index++;
+        System.out.println("添加成功");
+    }
+    public static void modify()
     {
     }
-    public Customer(int roomId,String name,String gender,String idNumber)
+    public static void delete()
     {
-        this.roomId=new Integer(roomId);
-        this.name=name;
-        this.gender=gender;
-        this.idNumber=idNumber;
+    }
+    public static void view()
+    {
+
     }
 
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setIdNumber(String idNumber) {
-        this.idNumber = idNumber;
-    }
-
-    public int getRoomId() {
-        return roomId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getIdNumber() {
-        return idNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "房间号:"+roomId+"\t"+"顾客姓名:"+name+"\t"+"性别:"+gender+"\t"+"身份证号码:"+idNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(roomId, customer.roomId) && Objects.equals(name, customer.name) && Objects.equals(gender, customer.gender) && Objects.equals(idNumber, customer.idNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(roomId, name, gender, idNumber);
-    }
 }
