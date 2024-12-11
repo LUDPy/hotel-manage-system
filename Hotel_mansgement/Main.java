c9cc2e7cc874247dc1d1e13b99b2f93488a73a63
Yingluomm <153310424+Yingluomm@users.noreply.github.com>
2024-12-11 21:22:43 +0800

    Update Main.java

--- Hotel_mansgement/Main.java
+++ Hotel_mansgement/Main.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 public class Main {
     static public void main(String[] args) {
 
+        System.out.println("顾客你好,本店所有房间都为空");
         Scanner sc=new Scanner(System.in);
         while(true)
         {
-            System.out.println("欢迎来到酒店管理系统");
+            System.out.println("---------------------商务酒店管理系统---------------------------------");
             System.out.println("1.添加顾客信息");
             System.out.println("2.修改顾客信息");
             System.out.println("3.删除顾客信息");
@@ -40,14 +41,19 @@ public class Main {
     }
     public static void add()
     {
+        if(Customer.index>=Customer.arrCustomer.length)
+        {
+            System.out.println("无空房间");
+            return;
+        }
         Scanner sc=new Scanner(System.in);
-        System.out.println("请输入房间号");
+        System.out.println("房间号为(1-50):");
         int roomId=sc.nextInt();
-        System.out.println("请输入姓名");
+        System.out.println("请输入姓名:");
         String name=sc.next();
-        System.out.println("请输入性别");
+        System.out.println("请输入性别(男/女):");
         String gender=sc.next();
-        System.out.println("请输入身份证号码");
+        System.out.println("请输入身份证号码:");
         String idNumber=sc.next();
         Customer.arrCustomer[Customer.index]=new Customer(roomId,name,gender,idNumber);
         System.out.println(Customer.arrCustomer[Customer.index].toString());
