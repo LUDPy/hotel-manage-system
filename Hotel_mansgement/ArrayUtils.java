48767f0cdf7d6d23956c9c99be27f28cc2c333dd
Heisenburg11 <134601214+Heisenburg11@users.noreply.github.com>
2024-12-27 10:53:17 +0800

    Update ArrayUtils.java

--- Hotel_mansgement/ArrayUtils.java
+++ Hotel_mansgement/ArrayUtils.java
@@ -3,14 +3,15 @@ package Hotel_mansgement;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class ArrayUtils {
-    public static int findCustomerByRoomId()
+public class ArrayUtils //工具类
+{
+    public static int findCustomerByRoomId()//通过房间号查找
     {
-        int iForReturn=-1;
+        int iForReturn=-1;//用来返回房间号，若房间没人则返回初始值：-1
         Scanner scanner=new Scanner(System.in);
         System.out.println("请输入房间号");
         //判断是否存在
-        boolean flag=true;
+        boolean flag=true;//标志房间是否没有人
         int roomId= scanner.nextInt();
         for (int i=0;i<Customer.index;i++)
         {
@@ -27,13 +28,13 @@ public class ArrayUtils {
         }
        return iForReturn;
     }
-    public static void findCustomerByName()
+    public static void findCustomerByName()//通过顾客名查找
     {
 
         Scanner scanner=new Scanner(System.in);
         System.out.println("请输入姓名");
         //判断是否存在
-        boolean flag=true;
+        boolean flag=true;//标志是否不存在此人
         String  name= scanner.next();
         for (int i=0;i<Customer.index;i++)
         {
@@ -49,12 +50,12 @@ public class ArrayUtils {
             System.out.println("查无此人");
         }
     }
-    public static void findCustomerByIdNumber()
+    public static void findCustomerByIdNumber()//通过身份证号码查询
     {
         Scanner scanner=new Scanner(System.in);
         System.out.println("请输入身份证号");
         //判断是否存在
-        boolean flag=true;
+        boolean flag=true;//标志是否不存在此人
         String  number= scanner.next();
         for (int i=0;i<Customer.index;i++)
         {
